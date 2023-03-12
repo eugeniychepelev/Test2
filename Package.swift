4c052380fc236981e27b01c7fce24a231d3ee790
evgeniyChepelev <e.chepelev@itransition.com>
2023-03-12 22:43:09 +0400

    df

--- Package.swift
+++ Package.swift
@@ -11,19 +11,21 @@ let package = Package(
             name: "LocalPushNotification",
             targets: ["LocalPushNotificationTarget"]),
         .library(name: "APNSPushNotification", targets: ["APNSPushNotificationTarget"]),
-//        .library(name: "FirebasePushNotification", targets: ["FirebasePushNotificationTarget"])
+        .library(name: "FirebasePushNotification", targets: ["FirebasePushNotificationTarget"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "10.4.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LocalPushNotificationTarget",
             dependencies: [],
             path: "Sources/LocalPushNotificationTarget"),
+        .target(name: "FirebasePushNotificationTarget",
+               dependencies: [
+                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk")
+               ],
+               path: "Sources/FirebasePushNotificationTarget"),
         .target(name: "APNSPushNotificationTarget",
                dependencies: [],
                path: "Sources/APNSPushNotificationTarget"),
